798efecb40c26a5ec64782ec6016f09556cc1745
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-09 05:50:53 -0700

    Pastas reestruturadas

--- FourBank/RegisterScreen1/View/RegisterScreen1ViewController.swift
+++ FourBank/RegisterScreen1/View/RegisterScreen1ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class RegisterScreen1ViewController: UIViewController {
+    
+    let registerScreen1ViewModel = RegisterScreen1ViewModel()
 
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -19,7 +21,12 @@ class RegisterScreen1ViewController: UIViewController {
        
             }
 
-
+    @IBAction func proceedButton(_ sender: UIButton) {
+        registerScreen1ViewModel.registerPersonalInfo(firstName: firstNameTextField.text, lastName: lastNameTextField.text, cpf: cpfTextField.text, rg: rgTextField.text)
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+    }
     /*
     // MARK: - Navigation
 
