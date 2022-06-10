aeeefed73ba6b58ce58f3aa2f9f7527d2cd7c74f
HenriRodriguesS <105744193+HenriRodriguesS@users.noreply.github.com>
2022-06-10 09:25:23 -0700

    Ligações feitas da BalanceCell.

--- FourBank/StatementScreen/View/Cell/BalanceCell.swift
+++ FourBank/StatementScreen/View/Cell/BalanceCell.swift
@@ -2,13 +2,18 @@
 //  BalanceCell.swift
 //  FourBank
 //
-//  Created by user220210 on 6/10/22.
+//  Created by Henrique Rodrigues Silva on 6/10/22.
 //
 
 import UIKit
 
 class BalanceCell: UITableViewCell {
 
+    @IBOutlet weak var userLabel:             UILabel!
+    
+    @IBOutlet weak var transferredValueLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
