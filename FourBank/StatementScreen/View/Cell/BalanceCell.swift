64d733906691cebe66662f571e2a2da22cac4b06
HenriRodriguesS <105744193+HenriRodriguesS@users.noreply.github.com>
2022-06-10 07:37:36 -0700

    Construção da tela de Extrato (StatementScreen) e da célula da TableView do extrato.

---
+++ FourBank/StatementScreen/View/Cell/BalanceCell.swift
@@ -0,0 +1,23 @@
+//
+//  BalanceCell.swift
+//  FourBank
+//
+//  Created by user220210 on 6/10/22.
+//
+
+import UIKit
+
+class BalanceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
