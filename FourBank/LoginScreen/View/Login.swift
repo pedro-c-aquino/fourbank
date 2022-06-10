b7efcb914fcd191151b028caf936793f2984ffe4
HenriRodriguesS <105744193+HenriRodriguesS@users.noreply.github.com>
2022-06-10 05:58:38 -0700

    Alterações feitas para os Segue's da tela de Login.

--- FourBank/LoginScreen/View/Login.swift
+++ FourBank/LoginScreen/View/Login.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class Login: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField:    UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginButton(_ sender: UIButton) {
+            
+            if (emailTextField.text == "" && passwordTextField.text == "") {
+                
+                print("Campos vazios.")
+                
+            } else if (emailTextField.text == "" || passwordTextField.text == ""){
+                
+                print("E-mail ou senha inválidos.")
+                
+            } else {
+                
+                performSegue(withIdentifier: "toHome", sender: nil)
+            }
+            
+        }
+        
+        
+        
+        @IBAction func registerButton(_ sender: UIButton) {
+            
+            performSegue(withIdentifier: "toRegister", sender: nil)
+        }
+    
 
 }
 
