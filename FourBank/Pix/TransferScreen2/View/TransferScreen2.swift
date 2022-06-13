a0e65959ffe837cb563e35662e0e6986e6d2739d
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-13 07:46:40 -0700

    Criada a célula da TableView de contatos PIX

--- FourBank/Pix/TransferScreen2/View/TransferScreen2.swift
+++ FourBank/Pix/TransferScreen2/View/TransferScreen2.swift
@@ -13,13 +13,23 @@ class TransferScreen2: UIViewController {
     @IBOutlet weak var pixKeyTextField: UITextField!
     @IBOutlet weak var contactsTableView: UITableView!
     
+    var contatos: [String] = [
+        "Pedro",
+        "Henrique",
+        "Kaue"
+    ]
+    
     var transferAmount: Double = 0.00
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         print(transferAmount)
-        // Do any additional setup after loading the view.
+        contactsTableView.delegate = self
+        contactsTableView.dataSource = self
+        
     }
+    
 
 
     /*
@@ -32,4 +42,26 @@ class TransferScreen2: UIViewController {
     }
     */
 
+}
+
+extension TransferScreen2: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        contatos.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = contactsTableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        cell.textLabel?.text = contatos[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "TrScreen2ToTrScreen3", sender: self)
+    }
+    
+    
 }
