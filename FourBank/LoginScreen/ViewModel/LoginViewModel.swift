c28106a245b6fe3fe511bd9e35bacbf93fe49ac7
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-13 11:44:55 -0700

    Inserida a autenticação de login com o Firebase

--- FourBank/LoginScreen/ViewModel/LoginViewModel.swift
+++ FourBank/LoginScreen/ViewModel/LoginViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class LoginViewModel {
+    
+    func validateLoginData(email: String?, password: String?) -> (email: String, password: String, validation: Bool) {
+        
+        if let email = email, let password = password {
+            return (email: email, password: password, validation: true)
+        } else {
+            return (email: "", password: "", validation: false)
+        }
+    }
+}
