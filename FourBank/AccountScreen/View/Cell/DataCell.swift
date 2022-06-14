b3c25b67bad8d25dcab564648a4e64638feb6a54
HenriRodriguesS <105744193+HenriRodriguesS@users.noreply.github.com>
2022-06-14 09:50:27 -0700

    Pequena alteração no texto do botão da tela de Registro 2.

--- FourBank/AccountScreen/View/Cell/DataCell.swift
+++ FourBank/AccountScreen/View/Cell/DataCell.swift
@@ -33,11 +33,7 @@ class DataCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    
     
     
 }
