97462e321f818dec9dea0bde74da3fbbd6de2c7c
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-15 07:40:36 -0700

    Refatorado o nome da pasta pixScreen1

---
+++ FourBank/Pix/PixScreen1/View/PixScreen1VC.swift
@@ -0,0 +1,58 @@
+//
+//  TransferScreen1.swift
+//  FourBank
+//
+//  Created by user220237 on 6/1/22.
+//
+
+import UIKit
+
+class PixScreen1VC: UIViewController {
+    
+    var transferAmount: Double = 0
+    
+    let transferScreen1VM = PixScreen1VM()
+
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        amountTextField.becomeFirstResponder()
+    }
+
+    @IBAction func closeButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func proceedButton(_ sender: UIButton) {
+        
+        let amountString = amountTextField.text
+        
+        transferAmount = transferScreen1VM.getAmount(amountText: amountString)
+        
+        
+        performSegue(withIdentifier: "TrScreen1ToTrScreen2", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as? PixScreen2
+        vc?.transferAmount = transferAmount
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
