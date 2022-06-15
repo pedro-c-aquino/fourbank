071c74c528e2b822520e8a8baa5d173e0513dfc2
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-15 11:26:51 -0700

    Criada a TransferScreen2 com ligações e segues

--- FourBank/Transfer/TransferScreen2/View/TransferScreen2.swift
+++ FourBank/Transfer/TransferScreen2/View/TransferScreen2.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class TransferScreen2: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var bankOfficeTextField: UITextField!
+    @IBOutlet weak var bankAccountTextField: UITextField!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
     }
-
+    
+    @IBAction func proceedButton(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "TrScreen2ToTrScreen3", sender: self)
+        
+    }
+    
 }
