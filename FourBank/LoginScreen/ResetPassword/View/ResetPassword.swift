0f1ad491f2fd05de39e1c924ba421b5cad331901
Kauê Jacyntho <kauekaj@gmail.com>
2022-06-17 08:07:38 -0700

    Finalizando configurações da ResetPassword e criando segues entre telas.

--- FourBank/LoginScreen/ResetPassword/View/ResetPassword.swift
+++ FourBank/LoginScreen/ResetPassword/View/ResetPassword.swift
@@ -5,4 +5,21 @@
 //  Created by user220237 on 6/17/22.
 //
 
-import Foundation
+import UIKit
+
+class ResetPAssword: UIViewController {
+    
+    
+    @IBOutlet weak var cpfTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func resetPasswordButton(_ sender: UIButton) {
+    }
+}
