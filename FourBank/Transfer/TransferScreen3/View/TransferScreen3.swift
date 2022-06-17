7b6791174e6b3e93bd83bab8915e93cddf5bc4c9
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-17 06:15:02 -0700

    Criada a TransferScreen3 com ligações.

--- FourBank/Transfer/TransferScreen3/View/TransferScreen3.swift
+++ FourBank/Transfer/TransferScreen3/View/TransferScreen3.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class TransferScreen3: UIViewController {
 
+    @IBOutlet weak var transferInfoTableView: UITableView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        transferInfoTableView.delegate = self
+        transferInfoTableView.dataSource = self
+        transferInfoTableView.register(UINib(nibName: "transferDataCell", bundle: nil), forCellReuseIdentifier: "transferDataCell")
     }
 
+    @IBAction func confirmButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+    }
+}
+
+extension TransferScreen3: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: transferDataCell? = transferInfoTableView.dequeueReusableCell(withIdentifier: "transferDataCell", for: indexPath) as? transferDataCell
+        cell?.cpfLabel.isHidden = true
+        cell?.cpfTitleLabel.isHidden = true
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    
 }
