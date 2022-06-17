642dc3eda6388c161923904b8299eacc2623693d
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-17 09:25:11 -0700

    Retirado o Firebase

--- FourBank/LoginScreen/Login/View/Login.swift
+++ FourBank/LoginScreen/Login/View/Login.swift
@@ -6,49 +6,33 @@
 //
 
 import UIKit
-import Firebase
 
 class Login: UIViewController {
     
     let loginViewModel = LoginViewModel()
-    
     @IBOutlet weak var emailTextField:    UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
     }
-
+    
     @IBAction func loginButton(_ sender: UIButton) {
         
         let loginInfo = loginViewModel.validateLoginData(email: emailTextField.text, password: passwordTextField.text)
-        
         if loginInfo.validation {
             
             CurrentUser.currentUserEmail = loginInfo.email
-            
-            Auth.auth().signIn(withEmail: loginInfo.email, password: loginInfo.password) { authResult, error in
-                if let e = error {
-                    print(e.localizedDescription)
-                } else {
-                    self.performSegue(withIdentifier: "toHome", sender: nil)
-                }
-            }
-            
-                
-            }
-            
+            performSegue(withIdentifier: "toHome", sender: nil)
         }
         
-        
-        
-        @IBAction func registerButton(_ sender: UIButton) {
-            
-            performSegue(withIdentifier: "toRegister", sender: nil)
-        }
+    }
     
-
+    @IBAction func registerButton(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "toRegister", sender: nil)
+    }
 }
 
--- FourBank/Register/RegisterScreen3/View/RegisterScreen3.swift
+++ FourBank/Register/RegisterScreen3/View/RegisterScreen3.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Firebase
 
 class RegisterScreen3: UIViewController {
     
