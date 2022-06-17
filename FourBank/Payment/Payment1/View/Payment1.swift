e281de9b7c35b648353c8109bd874cdacd8dd14f
HenriRodriguesS <105744193+HenriRodriguesS@users.noreply.github.com>
2022-06-17 07:08:11 -0700

    Alterações na tela de Pagamento, ligações dos elementos no código.

--- FourBank/Payment/Payment1/View/Payment1.swift
+++ FourBank/Payment/Payment1/View/Payment1.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class Payment1: UIViewController {
-
+    
+    
+    @IBOutlet weak var balanceLabel:         UILabel!
+    
+    @IBOutlet weak var amountTextField:  UITextField!
+    
+    @IBOutlet weak var barcodeTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func confirmButton(_ sender: UIButton) {
+        
+        
+    }
+    
 }
