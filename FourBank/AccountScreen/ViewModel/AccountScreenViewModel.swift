e07379d578c964eb564ba5925d47f8cfa6cd55cf
Kauê Jacyntho <kauekaj@gmail.com>
2022-06-20 12:10:04 -0700

    Criando arquivos de Network

--- FourBank/AccountScreen/ViewModel/AccountScreenViewModel.swift
+++ FourBank/AccountScreen/ViewModel/AccountScreenViewModel.swift
@@ -9,27 +9,6 @@ import Foundation
 
 class AccountScreenViewModel {
     
-    func networkUser(completionHandler: @escaping ([AccountModel]?, Error?) -> Void) -> Void {
-       
-        guard let url = URL(string: "https://62ad2075402135c7acbce26b.mockapi.io/api/v1/account2") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: url) { data, response, error in
-            if let data = data {
-                do {
-                    
-                    let jsonArray = try JSONDecoder().decode([AccountModel].self, from: data)
-                    completionHandler(jsonArray, nil)
-                    
-                } catch {
-                    completionHandler(nil, error)
-                }
-            }
-        }
-        task.resume()
-    }
+    
 }
 
