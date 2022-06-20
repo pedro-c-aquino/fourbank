6150eef7d32b4bb77af332b7f6c96b8e4b8ba25a
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-20 07:50:24 -0700

    Criada a função para receber o User da API

--- FourBank/AccountScreen/ViewModel/AccountScreenViewModel.swift
+++ FourBank/AccountScreen/ViewModel/AccountScreenViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class AccountScreenViewModel {
+    
+    func getUser() -> [String: Any]? {
+        var userDict: [String: Any]?
+        guard let url = URL(string: "https://api.chucknorris.io/jokes/random") else {
+            print("Invalid URL")
+            return nil
+        }
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dict = json as? [String: Any] {
+                        //FOR SINGLE VALUES
+                            DispatchQueue.main.async {
+                                userDict = dict
+                            }
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+        return userDict
+    }
+}
