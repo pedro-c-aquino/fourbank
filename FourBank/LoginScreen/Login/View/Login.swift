edc8cf5ca67976ea2892abd0c06d9e2c6e25a82f
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-06-22 06:47:32 -0700

    Refatorada a tela de Login para a arquitetura MVVM

--- FourBank/LoginScreen/Login/View/Login.swift
+++ FourBank/LoginScreen/Login/View/Login.swift
@@ -22,49 +22,7 @@ class Login: UIViewController {
     
     @IBAction func loginButton(_ sender: UIButton) {
         
-        let loginInfo = loginViewModel.validateLoginData(email: emailTextField.text, password: passwordTextField.text)
-        if loginInfo.validation {
-            
-            CurrentUser.currentUserEmail = loginInfo.email
-            network.networkUser { userArray, error in
-                if let userArray = userArray {
-                    for user in userArray {
-                        if CurrentUser.currentUserEmail == user.email {
-                            if loginInfo.password == user.password {
-                                
-                                DispatchQueue.main.async {
-                                    
-                                    self.performSegue(withIdentifier: "toHome", sender: nil)
-                                }
-                            } else {
-                                
-                                DispatchQueue.main.async {
-                                    
-                                    let alert = UIAlertController(title: "Senha inválida", message: "A senha digitada não confere com a senha cadastrada para este usuário.", preferredStyle: .alert)
-                                    let ok = UIAlertAction(title: "Ok", style: .default)
-                                    alert.addAction(ok)
-                                    self.present(alert, animated: true, completion: nil)
-                                }
-                            }
-                        } else {
-                            
-                            DispatchQueue.main.async {
-                                
-                                let alert = UIAlertController(title: "Usuário não cadastrado.", message: "Este usuário não foi encontrado no sistema, cadastre-se para acessar o app.", preferredStyle: .alert)
-                                let ok = UIAlertAction(title: "Ok", style: .default)
-                                alert.addAction(ok)
-                                self.present(alert, animated: true, completion: nil)
-                            }
-                        }
-                    }
-                }
-            }
-            
-            print(network.userModel)
-            
-            
-            
-        }
+        loginViewModel.validateLoginData(email: emailTextField.text, password: passwordTextField.text, vc: self)
     }
     
     @IBAction func registerButton(_ sender: UIButton) {
